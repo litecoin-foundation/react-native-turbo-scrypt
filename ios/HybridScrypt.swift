3a1b8466d202f0262ac40dd97432b06fa4f530e1
losh11 <loshan1212@gmail.com>
2025-01-26 03:44:57 +0000

    update nitro-modules to 0.21 & v0.0.5

--- ios/HybridScrypt.swift
+++ ios/HybridScrypt.swift
@@ -3,28 +3,19 @@ import CryptoSwift
 import NitroModules
 
 class HybridScrypt: HybridScryptSpec {
-    var hybridContext = margelo.nitro.HybridContext()
-    public override var memorySize: Int {
-        return getSizeOf(self)
-    }
-
     func scrypt(password: ArrayBufferHolder, salt: ArrayBufferHolder, N: Double, r: Double, p: Double, size: Double) throws -> ArrayBufferHolder {
-        // Convert ArrayBufferHolder to Array<UInt8>
-        let passwordArray = Array(UnsafeBufferPointer(start: password.data.assumingMemoryBound(to: UInt8.self), count: password.size))
-        let saltArray = Array(UnsafeBufferPointer(start: salt.data.assumingMemoryBound(to: UInt8.self), count: salt.size))
+        // Convert ArrayBufferHolder to Data
+        let passwordData = Data(bytes: password.data, count: password.size)
+        let saltData = Data(bytes: salt.data, count: salt.size)
         
-        do {
-            let key = try Scrypt(password: passwordArray, salt: saltArray, dkLen: Int(size), N: Int(N), r: Int(r), p: Int(p)).calculate()
-            
-            // Convert the result back into an ArrayBufferHolder
-            let resultData = UnsafeMutablePointer<UInt8>.allocate(capacity: key.count)
-                resultData.initialize(from: key, count: key.count)
-                    
-            return ArrayBufferHolder.wrap(dataWithoutCopy: resultData, size: key.count) {
-                resultData.deallocate()
-            }
-        } catch {
-            throw error
+        let key = try Scrypt(password: passwordData.bytes, salt: saltData.bytes, dkLen: Int(size), N: Int(N), r: Int(r), p: Int(p)).calculate()
+        
+        // Convert the result back into an ArrayBufferHolder
+        let keyPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: key.count)
+        keyPointer.initialize(from: key, count: key.count)
+        
+        return ArrayBufferHolder.wrap(dataWithoutCopy: keyPointer, size: key.count) {
+            keyPointer.deallocate()
         }
     }
 }
